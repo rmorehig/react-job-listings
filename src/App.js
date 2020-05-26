0dba5082afee95f097f90edd6d2d2eaf14343e32
Rafa Moreno <rmh1516@gmail.com>
2020-05-26 18:21:55 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import jobs from 'data/jobs'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <header></header>
+      <main>
+        <section>
+          <ul>
+            {jobs.map(job => (
+              <li key={job.id}>{job.company}</li>
+            ))}
+          </ul>
+        </section>
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
