17975055a585018a2c35f1e7ff7110c94e60593b
Rafa Moreno <rmh1516@gmail.com>
2020-05-28 10:31:41 +0200

    fix header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = styled.header`
   height: 156px;
   background-image: url(${props => props.backgroundDesktop});
   background-color: ${props => props.theme.colors.primary};
-  ${props => props.theme.breakpoints.mobile} {
+  ${props => props.theme.breakpoints.sm} {
     background-image: url(${props => props.backgroundMobile});
   }
 `
